b65d560d3babcfa8890c50e40cda21044e4e9e88
Kiarash Vosough <45785128+kiarashvosough1999@users.noreply.github.com>
2025-05-21 16:49:09 +0200

    added entities

---
+++ CoinWatch/DetailFeature/Entities/CoinDetailEntity.swift
@@ -0,0 +1,24 @@
+//
+//  CoinDetailEntity.swift
+//  CoinWatch
+//
+//  Created by Kiarash Vosough on 21.05.25.
+//
+
+import Foundation
+
+struct CoinDetailEntity: Identifiable {
+
+    let id: String
+    let symbol: String
+    let date: Date
+    let prices: [Price]
+
+    struct Price: Identifiable, Equatable {
+        let id: String
+        let price: Double
+        let cuurency: String
+    }
+}
+
+extension CoinDetailEntity: Equatable {}
