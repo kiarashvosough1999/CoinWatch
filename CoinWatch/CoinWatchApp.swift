dc9f95ea3a0f606e94609632695e798f77f0bb88
Kiarash Vosough <vosough.k@gmail.com>
2025-05-21 16:23:49 +0200

    Initial Commit

---
+++ CoinWatch/CoinWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinWatchApp.swift
+//  CoinWatch
+//
+//  Created by Kiarash Vosough on 21.05.25.
+//
+
+import SwiftUI
+
+@main
+struct CoinWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinWatch
+//
+//  Created by Kiarash Vosough on 21.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
